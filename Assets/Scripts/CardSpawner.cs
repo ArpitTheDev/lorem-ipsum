cddb318c90962a982b320af41d81122bb61a508a
as6611@gmail.com <as6611@gmail.com>
2025-07-24 23:46:49 +0530

    minor bug fixed total cards were not being set for persistantancy

--- Assets/Scripts/CardSpawner.cs
+++ Assets/Scripts/CardSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -76,6 +77,8 @@ public class CardSpawner : MonoBehaviour
         }
         else 
         {
+            TotalCards = Data.Rows * Data.Cols;
+
             foreach (var item in Data.CardsState)
             {
                 Card Card = Instantiate(CardPrefab, transform);
